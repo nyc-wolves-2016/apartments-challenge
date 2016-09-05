110de7b3b6bf96022b55601d365013bf1dee2e5a
Austin Lanari <email": null>
2016-09-05 16:30:49 -0400

    implement total sqft and price per sqft methods for apartment class

--- apartment.rb
+++ apartment.rb
@@ -1,2 +1,21 @@
 class Apartment
+  attr_reader :rooms, :monthly_rent
+  def initialize(args={})
+    @number = args.fetch(:number)
+    @monthly_rent = args.fetch(:monthly_rent, 1000)
+    @rooms = args.fetch(:rooms)
+  end
+
+  def room_count
+    @rooms.length
+  end
+
+  def total_sqft
+    @rooms.reduce(0) { |sum, room| sum + room.sqft }
+  end
+
+  def price_per_sqft
+    self.monthly_rent / total_sqft.to_f 
+  end
+
 end

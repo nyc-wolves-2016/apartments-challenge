88f802a5d0296747f96687bf32d2beb46492f393
Austin Lanari <email": null>
2016-09-05 16:40:53 -0400

    implement total room count and total monthly revenue methods for building

--- building.rb
+++ building.rb
@@ -1,2 +1,16 @@
 class Building
+  attr_reader :address, :apartments
+
+  def initialize(args={})
+    @address = args.fetch(:address)
+    @apartments = args.fetch(:apartments)
+  end
+
+  def total_room_count
+    @apartments.reduce(0) { |sum, apartments| sum + apartments.room_count }
+  end
+
+  def total_monthly_revenue
+    @apartments.reduce(0) { |sum, apartments| sum + apartments.monthly_rent }
+  end
 end

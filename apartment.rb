1401cbbb39db24c78617be035ff596dcb83a34a4
[01;31m[K"name"[m[K: "Jason Wong <[01;31m[K"email"[m[K: "jason.wong411@gmail.com>
2016-09-05 21:23:01 -0400

    Add room, apartment, building classes. Pass tests

--- apartment.rb
+++ apartment.rb
@@ -1,2 +1,29 @@
 class Apartment
+
+  attr_reader :number
+  attr_accessor :rooms, :monthly_rent
+
+  def initialize(args={})
+    @number = args.fetch(:number, "")
+    @rooms = args.fetch(:rooms, "")
+    @monthly_rent = args.fetch(:monthly_rent, 1000)
+  end
+
+  def total_sqft
+    area = @rooms.map { |room| room.sqft }
+    area.reduce(:+)
+  end
+
+  def price_per_sqft
+    @monthly_rent / total_sqft.to_f
+  end
+
+  def room_count
+    @rooms.length
+  end
+
+  def bedroom_count
+    bedrooms = @rooms.select { |room| room.name == "bedroom"}
+    bedrooms.length
+  end
 end

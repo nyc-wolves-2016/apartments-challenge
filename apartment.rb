f13712db1ffc74e291b0531955a5666453157ce5
Angel Chen <rikugou@gmail.com>
2016-09-05 22:07:27 -0400

    Add apartment class and pass rspec

--- apartment.rb
+++ apartment.rb
@@ -1,2 +1,25 @@
 class Apartment
+attr_reader :number, :rooms, :monthly_rent
+
+  def initialize(args={})
+    @number = args.fetch(:number, "")
+    @rooms = args.fetch(:rooms, "")
+    @monthly_rent = args.fetch(:monthly_rent, 1000)
+  end
+
+  def total_sqft
+    rooms.inject(0) {|sum, room| sum + room.sqft}
+  end
+
+  def price_per_sqft
+    monthly_rent / total_sqft.to_f
+  end
+
+  def room_count
+    rooms.length
+  end
+
+  def bedroom_count
+    rooms.count {|rooms| rooms.name == 'bedroom'}
+  end
 end
--- room.rb
+++ room.rb
@@ -1,2 +1,9 @@
 class Room
+attr_reader :name, :sqft, :window_count
+  def initialize(args={})
+    @name = args.fetch(:name)
+    @sqft = args.fetch(:sqft)
+    @window_count = args.fetch(:window_count)
+  end
+  
 end

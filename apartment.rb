2cbce7afb33d05f2280063c55ad837cd0033d2d5
Nan Min <nanutza@gmail.com>
2016-09-05 20:04:22 -0400

    Define Class Room and Apartment

--- apartment.rb
+++ apartment.rb
@@ -1,2 +1,35 @@
+require_relative 'room'
+require 'pry'
+
 class Apartment
+	attr_reader :number, :rooms, :monthly_rent
+	attr_accessor :total_sqft
+	def initialize(args = {})
+		@number = args.fetch(:number, 0)
+		@rooms = args.fetch(:rooms, [])
+		@monthly_rent = args.fetch(:monthly_rent, 1000)
+		@total_sqft = 0
+	end
+
+	def generate_rooms
+		@rooms << Room.new
+	end
+
+	def total_sqft
+		@rooms.each {|room| @total_sqft += room.sqft}
+		@total_sqft
+	end
+
+
+	def price_per_sqft
+		self.monthly_rent/ self.total_sqft.to_f
+	end
+
+	def room_count
+		@rooms.length
+	end
+
+	def bedroom_count
+		@rooms.count('bedrooom')
+	end
 end
--- room.rb
+++ room.rb
@@ -1,2 +1,9 @@
 class Room
+	attr_reader :sqft, :window_count
+	attr_accessor :name 
+	def initialize(args = {})
+		@name = args.fetch(:name,'bedroom')
+		@sqft = args.fetch(:sqft, 120)
+		@window_count = args.fetch(:window_count, 0)
+	end
 end

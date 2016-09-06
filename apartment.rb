d878fd0910fd7a02ab7b0012926078af8b89594a
Amanda Hinchey <akhinchey@gmail.com>
2016-09-05 22:53:52 -0400

    complete challange

--- apartment.rb
+++ apartment.rb
@@ -1,2 +1,27 @@
 class Apartment
+
+  attr_reader :rooms, :monthly_rent
+
+  def initialize(args={})
+    @number = args.fetch(:number, '')
+    @rooms = args.fetch(:rooms, [])
+    @monthly_rent = args.fetch(:monthly_rent, 1000)
+  end
+
+  def total_sqft
+    rooms.reduce(0) { |total, room| total + room.sqft }
+  end
+
+  def price_per_sqft
+    monthly_rent / total_sqft.to_f 
+  end
+
+  def room_count
+    rooms.count 
+  end
+
+  def bedroom_count
+    rooms.count { |room| room.name == 'bedroom' }
+  end
+
 end
--- building.rb
+++ building.rb
@@ -1,2 +1,31 @@
 class Building
+
+  attr_reader :apartments
+
+  def initialize(args={})
+    @address = args.fetch(:address, '')
+    @apartments = args.fetch(:apartments, [])
+  end
+
+  def total_room_count
+    apartments.reduce(0) { |total, apartment| total + apartment.room_count }
+  end
+
+  def total_monthly_revenue
+    apartments.reduce(0) { |total, apartment| total + apartment.monthly_rent }
+  end
+
+  def apartments_by_rent
+    apartments.sort { |a, b| b.monthly_rent <=> a.monthly_rent }
+  end
+
+  def find_apartments_by_bedroom_count(num_of_bedrooms)
+    apartments.find { |apartment| apartment.bedroom_count == num_of_bedrooms }
+  end
+
+  def total_sqft
+    apartments.reduce(0) { |total, apartment| total + apartment.total_sqft }
+  end
+
+
 end

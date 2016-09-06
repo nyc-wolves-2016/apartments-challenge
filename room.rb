1c9477032d3eda6ecaf3e725a4a291655ed281ca
Angel Chen <rikugou@gmail.com>
2016-09-05 22:14:19 -0400

    Add room class attributes and pass rspec

--- room.rb
+++ room.rb
@@ -1,9 +1,9 @@
 class Room
-attr_reader :name, :sqft, :window_count
+attr_reader :sqft
+attr_accessor :name
+
   def initialize(args={})
-    @name = args.fetch(:name)
-    @sqft = args.fetch(:sqft)
-    @window_count = args.fetch(:window_count)
+    @name = args.fetch(:name, 'bedroom')
+    @sqft = args.fetch(:sqft, 120)
   end
-  
 end

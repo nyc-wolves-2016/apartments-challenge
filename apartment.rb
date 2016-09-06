388bafdd73e1a8782e04c72a751afe7afaebd921
Soren Zeliger <szeliger@oberlin.edu>
2016-09-05 20:03:25 -0400

    finishes apartments

--- apartment.rb
+++ apartment.rb
@@ -1,2 +1,27 @@
 class Apartment
+
+	attr_reader :rooms, :number, :monthly_rent
+
+	def initialize(args={})
+		@rooms = args.fetch(:rooms, 'blah')
+		@number = args.fetch(:number, '10#6')
+		@monthly_rent = args.fetch(:monthly_rent, 1000)
+	end
+
+	def total_sqft
+		rooms.reduce(0) { |sum, room| sum + room.sqft }
+	end
+
+	def price_per_sqft
+		monthly_rent / total_sqft.to_f
+	end
+
+	def room_count
+		rooms.length
+	end
+
+	def bedroom_count
+		rooms.count { |room| room.name == 'bedroom' }
+	end
+
 end
--- room.rb
+++ room.rb
@@ -1,2 +1,11 @@
 class Room
+
+	attr_accessor :name
+	attr_reader :sqft
+
+	def initialize(args={})
+		@sqft = args.fetch(:sqft, 120)
+		@name = args.fetch(:name, 'bedroom')
+	end
+
 end

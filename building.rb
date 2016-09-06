073e21b60aaa6275bc1d5824b69cdded3e0c0c92
Angel Chen & Thomas Graves <pair+angel-chen.thomaslogangraves@devbootcamp.com>
2016-09-06 14:15:33 -0400

    Refactored for single method use.

--- building.rb
+++ building.rb
@@ -7,15 +7,15 @@ class Building
 	end
 
 	def total_room_count
-		apartments.map {|apartment| apartment.room_count}.reduce(:+)
+		apartments.reduce(0) {|sum, apartment| sum + apartment.room_count}
 	end
 
 	def total_monthly_revenue
-		apartments.map {|apartment| apartment.monthly_rent}.reduce(:+)
+		apartments.reduce(0) {|sum, apartment| sum + apartment.monthly_rent}
 	end
 
 	def apartments_by_rent
-		apartments.sort_by {|apartment| apartment.monthly_rent}.reverse
+		apartments.sort {|apt1, apt2| apt2.monthly_rent <=> apt1.monthly_rent}
 	end
 
 	def find_apartments_by_bedroom_count(num)
@@ -23,6 +23,6 @@ class Building
 	end
 
 	def total_sqft
-		apartments.map {|apartment| apartment.total_sqft}.reduce(:+)
+		apartments.reduce(0) {|sum, apartment| sum + apartment.total_sqft}
 	end
 end

7347269b46a564ce930bcb8d18ecd396fab8606b
[01;31m[K"name"[m[K: "Tyler Sharp <[01;31m[K"email"[m[K: "tyler.v.sharp@gmail.com>
2016-09-06 16:48:33 -0400

    Add variables and methods to building class to pass tests.

--- building.rb
+++ building.rb
@@ -1,2 +1,27 @@
 class Building
+  attr_reader :address, :apartments
+  def initialize(args={})
+    @address = args.fetch(:address,nil)
+    @apartments = args.fetch(:apartments,[])
+  end
+
+  def total_room_count
+    apartments.map { |apartment| apartment.rooms.count }.reduce(:+)
+  end
+
+  def total_monthly_revenue
+    apartments.map { |apartment| apartment.monthly_rent }.reduce(:+)
+  end
+
+  def apartments_by_rent
+    apartments.sort { |a,b| b.monthly_rent <=> a.monthly_rent }
+  end
+
+  def find_apartments_by_bedroom_count(count)
+    apartments.find { |apartment| apartment.bedroom_count == count }
+  end
+
+  def total_sqft
+    apartments.map { |apartment| apartment.total_sqft }.reduce(:+)
+  end
 end
